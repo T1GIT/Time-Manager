76e3e88d48c976b482ed0074bf916dcbf8e6fd48
T1GIT <61010947+T1GIT@users.noreply.github.com>
2020-06-04 17:09:43 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from modules.security import crypting
 from modules.web_routes import *
 
-create_keys()  # Обновление RSA ключей
+crypting.create_keys()  # Обновление RSA ключей
 
 if __name__ == "__main__":
     tm.run(host='127.0.0.1', port=5000)
